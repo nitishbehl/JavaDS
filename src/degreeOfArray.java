49ab0a28dcb61a8b5a217ae14b5983781d274f5a
Nitish Behl <nitishbehl60@gmail.com>
2020-10-20 20:23:19 +0530

    19/10/2020

---
+++ src/degreeOfArray.java
@@ -0,0 +1,18 @@
+//public class degreeOfArray {
+//    public static int findShortestSubArray(int[] nums) {
+//        int count = 0;
+//        for (int i = 0; i < nums.length; i++) {
+//            for (int j = 1; j < nums.length; j++) {
+//                if (nums[i] == nums[j]) {
+//                    count++;
+//                }
+//            }
+//        }
+//        return count;
+//    }
+//
+//    public static void main(String[] args) {
+//        int[] nums = {1,2,2,3,1,4,2};
+//        System.out.println(findShortestSubArray(nums));
+//    }
+//}
---
+++ src/findArray.java
@@ -0,0 +1,20 @@
+
+public class findArray {
+    public static int findDisappearedNumbers(int[] nums) {
+        int sum = 0, n = nums.length, total = 0, result = 0;
+        for (int i = 0; i < n - 1; i++) {
+            sum = n * (n + 1) / 2;
+            total = total + nums[i];
+            result = sum - total;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 8, 9};
+        System.out.println(findDisappearedNumbers(arr));
+    }
+}
+
+
+
